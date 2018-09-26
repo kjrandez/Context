5f430f53e50a8895f188c6d72e8a63b101938277
Keegan <kjrandez@gmail.com>
2018-09-26 19:53:26 -0400

    Source tree sucks

--- browser/src/visual/sidepanel/pasteboard.js
+++ browser/src/visual/sidepanel/pasteboard.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 
 export default class Pasteboard extends Component
 {
+    componentWillMount() {
+        this.props.paste.connect(this);
+    }
+
+    componentWillUnmount() {
+        this.props.paste.disconnect(this);
+    }
+
     render() {
         return(
             <div className="sidepanel-tab-content">
-                <p>I am Pasteboard</p>
+                <p>Pasteboard id: {this.props.paste.id()}</p>
             </div>
         );
     }
