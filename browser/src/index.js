296214c37e7adcf3af71f4b9b18034a706c20029
Keegan Jay <kjrandez@gmail.com>
2019-05-07 21:55:51 -0400

    Minor changes due to switched tooling. Updated to use TypeScript.

--- browser/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-import {Top} from './visual';
-import App from './app.js';
-
-var app = new App();
-registerServiceWorker();
-ReactDOM.render(<Top app={app} />, document.getElementById('root'));
